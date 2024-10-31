64d8cbb66684c5606fa8dea30bbd2c32e32cf83e
Dominik Cebula <dominikcebula@gmail.com>
2024-10-31 23:05:51 +0100

    aws-samples-lambda-eventbridge-scheduler

--- deployment/lambda_scheduler.tf
+++ deployment/lambda_scheduler.tf
@@ -1,47 +1,17 @@
-resource "aws_scheduler_schedule" "lambda_scheduler" {
-  name       = "lambda-schedule"
-  group_name = "default"
-
-  flexible_time_window {
-    mode = "OFF"
-  }
-
+resource "aws_cloudwatch_event_rule" "lambda_schedule" {
+  name                = "lambda-schedule"
   schedule_expression = "rate(1 minute)"
-
-  target {
-    arn      = aws_lambda_function.java_lambda_function.arn
-    role_arn = aws_iam_role.scheduler_role.arn
-    input = jsonencode({ "input" : "Message from EventBridge Scheduler!" })
-  }
 }
 
-resource "aws_iam_role" "scheduler_role" {
-  name               = "event-bridge-scheduler-role"
-  assume_role_policy = data.aws_iam_policy_document.scheduler_role_policy_document.json
+resource "aws_cloudwatch_event_target" "lambda_schedule_event_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_schedule.name
+  target_id = "event-target-${var.lambda_function_name}"
+  arn       = aws_lambda_function.java_lambda_function.arn
 }
 
-data "aws_iam_policy_document" "scheduler_role_policy_document" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect = "Allow"
-    principals {
-      type = "Service"
-      identifiers = ["scheduler.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy" "eventbridge_invoke_policy" {
-  name   = "event-bridge-invoke-lambda-policy"
-  role   = aws_iam_role.scheduler_role.id
-  policy = data.aws_iam_policy_document.eventbridge_invoke_policy_document.json
-}
-
-data "aws_iam_policy_document" "eventbridge_invoke_policy_document" {
-  statement {
-    sid    = "AllowEventBridgeToInvokeLambda"
-    actions = ["lambda:InvokeFunction"]
-    effect = "Allow"
-    resources = [aws_lambda_function.java_lambda_function.arn]
-  }
+resource "aws_lambda_permission" "lambda-update-reporting" {
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_schedule.arn
 }
